dd710c427a1092d50b567cbb92c9a3e14ffca0c5
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2019-05-01 23:44:09 +0900

    fix nearest neighbor

--- lib/rumale/nearest_neighbors/k_neighbors_classifier.rb
+++ lib/rumale/nearest_neighbors/k_neighbors_classifier.rb
@@ -20,15 +20,15 @@ module Rumale
       include Base::Classifier
 
       # Return the prototypes for the nearest neighbor classifier.
-      # @return [Numo::DFloat] (shape: [n_samples, n_features])
+      # @return [Xumo::DFloat] (shape: [n_samples, n_features])
       attr_reader :prototypes
 
       # Return the labels of the prototypes
-      # @return [Numo::Int32] (size: n_samples)
+      # @return [Xumo::Int32] (size: n_samples)
       attr_reader :labels
 
       # Return the class labels.
-      # @return [Numo::Int32] (size: n_classes)
+      # @return [Xumo::Int32] (size: n_classes)
       attr_reader :classes
 
       # Create a new classifier with the nearest neighbor rule.
@@ -46,30 +46,30 @@ module Rumale
 
       # Fit the model with given training data.
       #
-      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The training data to be used for fitting the model.
-      # @param y [Numo::Int32] (shape: [n_samples]) The labels to be used for fitting the model.
+      # @param x [Xumo::DFloat] (shape: [n_samples, n_features]) The training data to be used for fitting the model.
+      # @param y [Xumo::Int32] (shape: [n_samples]) The labels to be used for fitting the model.
       # @return [KNeighborsClassifier] The learned classifier itself.
       def fit(x, y)
         x = check_convert_sample_array(x)
         y = check_convert_label_array(y)
         check_sample_label_size(x, y)
-        @prototypes = Numo::DFloat.asarray(x.to_a)
-        @labels = Numo::Int32.asarray(y.to_a)
-        @classes = Numo::Int32.asarray(y.to_a.uniq.sort)
+        @prototypes = Xumo::DFloat.asarray(x.to_a)
+        @labels = Xumo::Int32.asarray(y.to_a)
+        @classes = Xumo::Int32.asarray(y.to_a.uniq.sort)
         self
       end
 
       # Calculate confidence scores for samples.
       #
-      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The samples to compute the scores.
-      # @return [Numo::DFloat] (shape: [n_samples, n_classes]) Confidence scores per sample for each class.
+      # @param x [Xumo::DFloat] (shape: [n_samples, n_features]) The samples to compute the scores.
+      # @return [Xumo::DFloat] (shape: [n_samples, n_classes]) Confidence scores per sample for each class.
       def decision_function(x)
         x = check_convert_sample_array(x)
         distance_matrix = PairwiseMetric.euclidean_distance(x, @prototypes)
         n_samples, n_prototypes = distance_matrix.shape
         n_classes = @classes.size
         n_neighbors = [@params[:n_neighbors], n_prototypes].min
-        scores = Numo::DFloat.zeros(n_samples, n_classes)
+        scores = Xumo::DFloat.zeros(n_samples, n_classes)
         n_samples.times do |m|
           neighbor_ids = distance_matrix[m, true].to_a.each_with_index.sort.map(&:last)[0...n_neighbors]
           neighbor_ids.each { |n| scores[m, @classes.to_a.index(@labels[n])] += 1.0 }
@@ -79,13 +79,13 @@ module Rumale
 
       # Predict class labels for samples.
       #
-      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The samples to predict the labels.
-      # @return [Numo::Int32] (shape: [n_samples]) Predicted class label per sample.
+      # @param x [Xumo::DFloat] (shape: [n_samples, n_features]) The samples to predict the labels.
+      # @return [Xumo::Int32] (shape: [n_samples]) Predicted class label per sample.
       def predict(x)
         x = check_convert_sample_array(x)
         n_samples = x.shape.first
         decision_values = decision_function(x)
-        Numo::Int32.asarray(Array.new(n_samples) { |n| @classes[decision_values[n, true].max_index] })
+        Xumo::Int32.asarray(Array.new(n_samples) { |n| @classes[decision_values[n, true].max_index.to_i] })
       end
 
       # Dump marshal data.
